9c5b5013a3fc370754f0f848f4c23fee0a9b84b0
Abby Kelly <akindred4@ivytech.edu>
2022-03-16 10:33:11 -0400

    fixed the chore generator had to add a second button and function

--- src/components/RoomsAndChores/DisplayGenerator.tsx
+++ src/components/RoomsAndChores/DisplayGenerator.tsx
@@ -6,8 +6,8 @@ import {
   IChores,
   IRoomGetAllResponse,
 } from "./RoomTable/room.getall.interface";
-import APIURL from '../../helpers/environment'
-import { Badge, Button, ListGroup } from "react-bootstrap";
+import APIURL from "../../helpers/environment";
+import { Badge, Button, CloseButton, ListGroup, Modal } from "react-bootstrap";
 
 interface DisplayGeneratorProps {
   token: string | null;
@@ -61,8 +61,8 @@ class DisplayGenerator extends React.Component<
     this.fetchForRandom();
   };
 
+  //! Choose a room button ----------------------------------------------
   generateRooms = () => {
-    // console.log(this.state.selectedRoomId);
     return this.state.rooms.map((room, index) => (
       <Dropdown.Item
         onClick={() =>
@@ -78,7 +78,8 @@ class DisplayGenerator extends React.Component<
     ));
   };
 
-  generateChore() {
+  //! Generate a specific chore button ----------------------------------
+  generateSpecific() {
     console.log(this.state.chosenTime, "TIME FROM BUTTON");
     const allChores = this.state.rooms.reduce(
       (chores, room) => [...chores, ...(room.chores || [])],
@@ -90,19 +91,40 @@ class DisplayGenerator extends React.Component<
         chore.roomId === this.state.selectedRoomId
       );
     });
-
-    console.log(generatorRules, "RULES");
-
     const randomNum = [Math.floor(Math.random() * generatorRules.length)];
     const choreIndex = Number(randomNum);
     const randomChore = generatorRules[choreIndex];
-    this.setState({ ...this.state, generatedChore: randomChore });
+    this.setState({ ...this.state, generatedChore: randomChore, show: true });
+
+    console.log(generatorRules, "RULES");
+    console.log(allChores, "all chores");
+    console.log(randomNum, "random num");
+    console.log(choreIndex, "chore index");
+    console.log(randomChore, "random chore");
+  }
+
+  //! Generate a random chore button ----------------------------------
+  generateRandom() {
+    console.log(this.state.chosenTime, "TIME FROM BUTTON");
+    const allChores = this.state.rooms.reduce(
+      (chores, room) => [...chores, ...(room.chores || [])],
+      [] as IChores[]
+    );
+
+    const randomNum = [Math.floor(Math.random() * allChores.length)];
+    const choreIndex = Number(randomNum);
+    const randomChore = allChores[choreIndex];
+    this.setState({ ...this.state, generatedChore: randomChore, show: true });
 
     console.log(allChores, "all chores");
     console.log(randomNum, "random num");
     console.log(choreIndex, "chore index");
     console.log(randomChore, "random chore");
   }
+
+  handleClose = () => {
+    this.setState({ show: false });
+  };
 
   render() {
     return (
@@ -115,15 +137,31 @@ class DisplayGenerator extends React.Component<
           fontFamily: "monospace",
         }}
       >
-        <h4 style={{ border: "solid 2px #1CA5B8", textAlign: "center" }}>
-          Choose a time-limit <strong>AND</strong> a room to get a more specific
-          random chore.
-          <br />
-          <strong>OR</strong>
-          <br />
-          click 'Generate chore' to get a completely random chore!
-        </h4>
         <div>
+          <h4 style={{ border: "solid 2px #1CA5B8", textAlign: "center" }}>
+            Click 'Generate Random' to get a completely random chore
+          </h4>
+          <Button
+            variant="warning"
+            size="lg"
+            style={{
+              marginLeft: "22vw",
+              marginTop: "6vh",
+              fontFamily: "monospace",
+            }}
+            onClick={() => this.generateRandom()}
+          >
+            Generate Random!
+          </Button>
+        </div>
+            <br />
+            <br />
+            <br />
+        <div>
+          <h4 style={{ border: "solid 2px #1CA5B8", textAlign: "center" }}>
+            Choose a time-limit <strong>AND</strong> a room to get a more specific
+            random chore.
+          </h4>
           <DropdownButton
             id="dropdown-button-dark-example2"
             variant="secondary"
@@ -151,9 +189,7 @@ class DisplayGenerator extends React.Component<
             </Dropdown.Item>
           </DropdownButton>
         </div>
-
-          <h3>time limit: {this.state.chosenTime}minutes</h3>
-
+        <h3>time limit: {this.state.chosenTime}minutes</h3>
         <div style={{ paddingTop: "5vh" }}>
           <DropdownButton
             id="dropdown-button-dark-example2"
@@ -165,9 +201,7 @@ class DisplayGenerator extends React.Component<
             {this.generateRooms()}
           </DropdownButton>
         </div>
-
-          <h3>selected room: {this.state.selectedRoomName}</h3>
-
+        <h3>selected room: {this.state.selectedRoomName}</h3>
         <div>
           <Button
             variant="warning"
@@ -177,7 +211,7 @@ class DisplayGenerator extends React.Component<
               marginTop: "6vh",
               fontFamily: "monospace",
             }}
-            onClick={() => this.generateChore()}
+            onClick={() => this.generateSpecific()}
           >
             Generate Chore!
           </Button>
@@ -185,24 +219,31 @@ class DisplayGenerator extends React.Component<
 
         <div>
           <br />
-          {this.state.generatedChore && (
-            <ListGroup as="ol" numbered>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">
-                    {this.state.generatedChore.chore}
+          <Modal show={this.state.show} style={{ alignContent: "center" }}>
+            <CloseButton
+              style={{ marginLeft: "90%", fontFamily: "monospace" }}
+              onClick={() => this.handleClose()}
+              aria-label="Hide"
+            />
+            {this.state.generatedChore && (
+              <ListGroup as="ol" numbered>
+                <ListGroup.Item
+                  as="li"
+                  className="d-flex justify-content-between align-items-start"
+                >
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">
+                      {this.state.generatedChore.chore}
+                    </div>
+                    {this.state.generatedChore.desc}
                   </div>
-                  {this.state.generatedChore.desc}
-                </div>
-                <Badge bg="primary" pill>
-                  {this.state.generatedChore.time}mins
-                </Badge>
-              </ListGroup.Item>
-            </ListGroup>
-          )}
+                  <Badge bg="primary" pill>
+                    {this.state.generatedChore.time}mins
+                  </Badge>
+                </ListGroup.Item>
+              </ListGroup>
+            )}
+          </Modal>
         </div>
       </div>
     );
